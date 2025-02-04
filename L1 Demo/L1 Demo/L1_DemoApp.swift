e1f3c18e1fcc635cb5d18a75a7ae34c80559ca2c
Stella Tukia <148345190+st3lll4@users.noreply.github.com>
2025-02-04 17:13:00 +0200

    Add files via upload

---
+++ L1 Demo/L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Stella Tukia on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
